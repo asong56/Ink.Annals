457ded50cbf7ef1d646f3dfa7f9d9a917bd8fcaa
Ansel <238927804+Ansel-S@users.noreply.github.com>
2026-03-23 18:49:43 -0400

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+
+export default defineConfig({
+    plugins: [
+        viteSingleFile(),
+        ViteMinifyPlugin({
+            collapseWhitespace: true,
+            removeComments: true,
+            minifyCSS: true,
+            minifyJS: true,
+        }),
+    ],
+    build: {
+        target: "esnext",
+        assetsInlineLimit: 100000000,
+        minify: "terser", 
+        terserOptions: {
+            compress: {
+                drop_console: true,
+            },
+        },
+    },
+});
