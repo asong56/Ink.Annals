ec8816b81a09aa3412cfd7ae34012e5de5a252b6
Ansel <238927804+asong56@users.noreply.github.com>
2026-04-10 16:23:09 -0400

    Add PWA support with VitePWA plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
@@ -11,6 +12,30 @@ export default defineConfig({
             minifyCSS: true,
             minifyJS: true,
         }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Newspaper Generator',
+                short_name: 'Ink.Annals',
+                icons: [
+                    {
+                        src: 'assets/web-app-manifest-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                        "purpose": "maskable"
+                    },
+                    {
+                        src: 'assets/web-app-manifest-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        "purpose": "maskable"
+                    }
+                ],
+                "theme_color": "#f2f2f7",
+                "background_color": "#f2f2f7",
+                "display": "standalone"
+            }
+        })
     ],
     build: {
         target: "esnext",
